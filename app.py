18340fbe4c2dc08c35a8b7c0b7e1a27f6b915fb7
bankmotion <harris5555555555555@gmail.com>
2025-03-26 10:20:11 -0500

    Update host

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def home():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
